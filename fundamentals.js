a02403faa530fb17ed6ab0905bb9f671a36a82b5
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-10 09:12:10 -0400

    finsished fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -1,25 +1,27 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations.
 
 // Create an array of strings called `colors` that contains three colors.
-
-
+var colors = ['red', 'green', 'blue']
 
 // Access the last item in the array and assign to a variable called `last`.
-
-
+var last = colors[colors.length - 1]
 
 // Create an empty array called `favoriteColors`.
-
-
+favoriteColors = []
 
 // Create a `for` loop that adds each string in `colors` to `favoriteColors`.
-
-
+for (var i = 0; i < favoriteColors.length; i++) {
+  favoriteColors[i] = colors[i]
+}
 
 // Create an object literal called `instructor` that contains three key-value pairs.
-
-
+var instructor = {
+  name: 'Jim',
+  age: 32,
+  class: 'wdi17'
+}
 
 // Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
+instructor['facial-hair'] = true
